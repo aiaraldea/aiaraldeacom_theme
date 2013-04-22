5570ea7751e84cc6df0be1e212d2d7946f7c9b3f
Karlos g liberal <karlosgliberal@gmail.com>
2013-04-22 14:52:12 -0400

    no es necesario inicializar mediaelement, sino bug con ios

--- js/irratia.js
+++ js/irratia.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {  
-  $('audio').mediaelementplayer({audioWidth: 200});
   $('.view-irratia-feeds p').remove();
   $('.mejs-container').css("width: 200px; height: 30px;");
 });
