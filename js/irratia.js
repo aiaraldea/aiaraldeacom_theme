917852ae4636bd52e156deef5ef729870a6a5837
Karlos g liberal <karlosgliberal@gmail.com>
2013-05-22 04:04:51 -0400

    Cambios con el nuevo sitema de repodructor

--- js/irratia.js
+++ js/irratia.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {  
+  $('.mejs-container').css("width: 200px; height: 30px;");
   $('.view-irratia-feeds p').remove();
   $('.view-irratia-feeds ul').remove();
-  $('.mejs-container').css("width: 200px; height: 30px;");
+  $('.view-irratia-feeds script').remove();
+  $('.view-irratia-feeds object').remove();
 });
