215d3bdc272fe5e6027435e1c2729c456189fcc4
gorbeia <inakiibarrola@yahoo.es>
2015-01-21 14:21:58 +0100

    Albisteetan partekatzeko botoi berriak jarri

--- js/share.js
+++ js/share.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     $('#detail_share_links').append("<div class='facebook_share_button'><a target='_blank' href='http://www.facebook.com/sharer.php?u="+encodedUrl+"'>Facebook</a></div>");
     $('#detail_share_links').append("<div class='twitter_share_button'><a target='_blank' href='https://twitter.com/intent/tweet?text="+title+"&url="+encodedUrl+"&original_referer="+encodedUrl+"'>Twitter</a></div>");
     if (hasWhatsapp) {
-        $('#detail_share_links').append("<div class='whatsapp_share_button'><a href='whatsapp://send?text=<"+ title + " " + canonicalUrl+"' data-action='share/whatsapp/share'>WhatsApp</a></div>");
+        $('#detail_share_links').append("<div class='whatsapp_share_button'><a href='whatsapp://send?text="+ title + " " + canonicalUrl+"' data-action='share/whatsapp/share'>WhatsApp</a></div>");
     }
 });
 
