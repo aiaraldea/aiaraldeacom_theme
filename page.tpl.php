fd84c7a301eb1738e2991a5c56879a52c0838d0f
gorbeia <inakiibarrola@yahoo.es>
2015-01-22 14:41:44 +0100

    Javascript bukaeran kargatu

--- page.tpl.php
+++ page.tpl.php
@@ -88,7 +88,6 @@
 <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
   <?php print $head; ?>
   <?php print $styles; ?>
-  <?php print $scripts; ?>
   
   <link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
 <link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
@@ -311,6 +310,7 @@
     </div>
   <?php endif; ?>
 
+  <?php print $scripts; ?>
   <?php print $closure; ?>
 </body>
 </html>
